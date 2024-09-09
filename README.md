ffcb8d3b9b531f78addbd79724398c394bbcdd8a
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-09-09 15:50:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# project-name
-A description
+Orion JTensor
+Java implementation of PyTorch
--- src/test/java/io/github/orionlibs/orion_jtensor/ATest.java
+++ src/test/java/io/github/orionlibs/orion_jtensor/ATest.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.project_name;
+package io.github.orionlibs.orion_jtensor;
 
 import java.io.IOException;
 import java.util.TimeZone;
